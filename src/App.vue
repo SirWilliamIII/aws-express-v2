9639215894a8ed66f5da2a71b3c90f833aee49cf
SirWilliamIII <willcarpenterwastaken@gmail.com>
2020-10-20 01:39:42 -0500

    added email auth

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
   <div id="app">
-    <h1>Todo App</h1>
-    <input type="text" v-model="name" placeholder="Todo name">
-    <input type="text" v-model="description" placeholder="Todo description">
-    <button v-on:click="createTodo">Create Todo</button>
-    <div v-for="item in todos" :key="item.id">
-      <h3>{{ item.name }}</h3>
-      <p>{{ item.description }}</p>
-    </div>
+    <amplify-authenticator>
+      <h1>Todo App</h1>
+      <input type="text" v-model="name" placeholder="Todo name" />
+      <input type="text" v-model="description" placeholder="Todo description" />
+      <button v-on:click="createTodo">Create Todo</button>
+      <div v-for="item in todos" :key="item.id">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+      </div>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
-import { API } from 'aws-amplify';
-import { createTodo } from './graphql/mutations';
-import { listTodos } from './graphql/queries';
-import { onCreateTodo } from './graphql/subscriptions';
-
+import { API } from "aws-amplify";
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import { onCreateTodo } from "./graphql/subscriptions";
 
 export default {
-  name: 'App',
+  name: "App",
   async created() {
-    this.getTodos()
-    this.subscribe()
+    this.getTodos();
+    this.subscribe();
   },
   data() {
     return {
-      name: '',
-      description: '',
-      todos: []
-    }
+      name: "",
+      description: "",
+      todos: [],
+    };
   },
   methods: {
     async createTodo() {
@@ -39,28 +41,27 @@ export default {
       this.todos = [...this.todos, todo];
       await API.graphql({
         query: createTodo,
-        variables: {input: todo},
+        variables: { input: todo },
       });
-      this.name = '';
-      this.description = '';
+      this.name = "";
+      this.description = "";
     },
     async getTodos() {
       const todos = await API.graphql({
-        query: listTodos
+        query: listTodos,
       });
       this.todos = todos.data.listTodos.items;
     },
     subscribe() {
-      API.graphql({ query: onCreateTodo })
-        .subscribe({
-          next: (eventData) => {
-            let todo = eventData.value.data.onCreateTodo
-            if(this.todos.some(item => item.name === todo.name)) return
+      API.graphql({ query: onCreateTodo }).subscribe({
+        next: (eventData) => {
+          let todo = eventData.value.data.onCreateTodo;
+          if (this.todos.some((item) => item.name === todo.name)) return;
 
-            this.todos = [...this.todos, todo]
-          }
-        })
-    }
-  }
-}
+          this.todos = [...this.todos, todo];
+        },
+      });
+    },
+  },
+};
 </script>
